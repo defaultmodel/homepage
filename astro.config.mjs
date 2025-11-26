b6869f1723fe5c30d050121e7582660be9faaeb7
Hilan Meyran <hilanmeyran@protonmail.com>
2025-11-26 14:26:05 +0100

    tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,30 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import { autoNewTabExternalLinks } from './src/autoNewTabExternalLinks';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { autoNewTabExternalLinks } from "./src/autoNewTabExternalLinks";
 
 import partytown from "@astrojs/partytown";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://devolio.devaradise.com',
-  integrations: [mdx(), sitemap(), tailwind(), partytown()],
-  markdown: {
-    extendDefaultPlugins: true,
-    rehypePlugins: [[autoNewTabExternalLinks, {
-      domain: 'localhost:4321'
-    }]]
-  }
+	site: "https://devolio.devaradise.com",
+	integrations: [mdx(), sitemap(), partytown()],
+
+	markdown: {
+		extendDefaultPlugins: true,
+		rehypePlugins: [
+			[
+				autoNewTabExternalLinks,
+				{
+					domain: "localhost:4321",
+				},
+			],
+		],
+	},
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
