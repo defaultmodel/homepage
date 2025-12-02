61ca131be1a2eb259de907ce7866c996d5657825
Hilan Meyran <hilanmeyran@protonmail.com>
2025-12-02 10:08:06 +0100

    change constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TITLE = "Defaultmodel";
+export const SITE_TAGLINE = "Blog and portfolio"; // site tagline
+export const SITE_DESCRIPTION =
+	"Hilan Meyran portfolio and blog about cybersecurity"; // site meta description for SEO
