68e861ebe453a5d21d7892bea20356028169ca10
Hilan Meyran <hilanmeyran@protonmail.com>
2025-12-06 18:32:28 +0100

    working rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,25 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = await getCollection("blog");
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
-			...post.data,
-			link: `/${post.slug}/`,
+			title: post.title,
+			description: post.description,
+			pubDate: post.pubDate,
+			link: `/${post.id}/`,
+			content: sanitizeHtml(parser.render(post.body), {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+			}),
 		})),
 	});
 }
