3180603d397ed35ca934643b035db2a547a8c063
Hilan Meyran <hilanmeyran@protonmail.com>
2025-12-06 18:33:10 +0100

    page rework

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Defaultmodel";
 export const SITE_TAGLINE = "Blog and portfolio"; // site tagline
 export const SITE_DESCRIPTION =
-	"Hilan Meyran portfolio and blog about cybersecurity"; // site meta description for SEO
+	"Defaultmodel's portfolio and blog about cybersecurity"; // site meta description for SEO
