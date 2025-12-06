f562374f9774e52cfa19958f003ea39e3906a375
Hilan Meyran <hilanmeyran@protonmail.com>
2025-12-06 14:13:04 +0100

    change constant

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://devolio.devaradise.com",
+	site: "https://defaultmodel.eu.org",
 	integrations: [mdx(), sitemap(), partytown()],
 
 	markdown: {
